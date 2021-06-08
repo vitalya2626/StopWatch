5c9f2d06ec795c68f94170354397ab0d8f91a6f1
Vitalii Siedykh <siedykh.v@gmail.com>
2021-06-08 10:50:01 +0300

    Watch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import { interval } from "rxjs";
+import { map } from "rxjs/operators";
+import "./App.css";
+import StopWatch from "./Stopwatch/Stopwatch";
+
+const delay = 1000;
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [difference, setDifference] = useState(0);
+
+  const [subscription, setSubscription] = useState("");
+  const [prevent, setPrevent] = useState(true);
+
+  const onStartHandler = () => {
+    if (!subscription) {
+      const timerSubscription = interval(delay)
+        .pipe(map((v) => v + 1))
+        .subscribe((v) => {
+          setTimer(v + difference);
+        });
+      setSubscription(timerSubscription);
+    } else {
+      subscription.unsubscribe();
+      setTimer(0);
+      setDifference(0);
+      setSubscription("");
+    }
+  };
+
+  const pause = (event) => {
+    if (prevent) {
+      setPrevent(false);
+      const timerInstance = setTimeout(function () {
+        setPrevent(true);
+        clearTimeout(timerInstance);
+      }, 300);
+    } else {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+
+      setDifference(timer);
+      setSubscription("");
+    }
+  };
+
+  const onResetHandler = () => {
+    if (subscription) {
+      subscription.unsubscribe();
+    }
+
+    const timerSubscription = interval(delay).subscribe((v) => {
+      setTimer(v);
+    });
+    setSubscription(timerSubscription);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>RxJS Timer</h1>
+      <hr />
+      <StopWatch timePassed={timer ? timer : difference} />
+      <div>
+        <button onClick={onStartHandler} className="btn btn-success">
+          Start/Stop
+        </button>
+        <button 
+          onClick={pause} 
+          className="btn btn-warning"
+          title={'Use doubleclick to activate'}
         >
-          Learn React
-        </a>
-      </header>
+          Pause
+        </button>
+        <button onClick={onResetHandler} className="btn btn-danger">
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
